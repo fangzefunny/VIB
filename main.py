378109cec77f2057e35a69864de9c7e15cd2797e
Zeming <fangzefunny@gmail.com>
2022-03-28 11:29:03 -0400

    To be continued

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 # for MNIST download 
 import torch
 import torch.nn as nn 
+from torch.optim import Adam
 from torchvision import datasets, transforms
 
 # fro visualize
@@ -62,6 +63,8 @@ def _initLayer( layer, mode='xavier', bias=True):
         nn.init.constant_( layer.bias, 0)
     return layer
 
+ce = nn.CrossEntropyLoss()
+
 class VIB( nn.Module):
 
     def __init__( self, z_dim=128, n_latent=12, gpu=True):
@@ -82,10 +85,57 @@ class VIB( nn.Module):
         x = 2*x - 1 
         x = torch.relu( self.fc2( torch.relu( self.fc1( x))))
         mu, logsig = self.enc_mean(x), self.enc_std( x)
-        z_dim = mu.unsqueeze(1) + \
+        z = mu.unsqueeze(1) + \
                 torch.randn( (tuple( logsig.shape)[0],)+( self.n_latent,
                 )+tuple( mu.shape)[1:]).to( self.device) + \
-                logsig.unsqueeze(1)
+                logsig.exp().unsqueeze(1)
+        p_Y1Z = torch.softmax( self.dec(z), dim=2)
+        p_Y1X = p_Y1Z.mean(dim=1)
+        return p_Y1Z, p_Y1X, mu, logsig 
+
+    def get_loss( self, p_Y1Z, Y, mu, logsig, beta=1e-2):
+        '''Calculate the VIB object
+        '''
+        # reshape the target label Y to match dims (n_batch, n_samples)
+        batchSize, n_samples = p_Y1Z.shape[0], p_Y1Z.shape[1]
+        Y = Y.view(-1,1) * torch.ones( batchSize, n_samples, dtype=torch.long).to(self.device)
+        err = ce( p_Y1Z.view( -1, 10), Y.view( -1))
+        comp = .5 * (mu**2 + logsig.exp().pow() - 2*logsig - 1).sum(dim=1).mean()
+        return err + beta * comp 
+
+#--------------------------
+#      Train the VIB 
+#--------------------------
+
+def trainVIB( z_dim=128, lr=1e-4, beta=1e-2, 
+                batchSize=128, MaxEpoch=10, logfreq=1):
+
+    # get train and test data 
+    train_data, test_data = get_MNIST(batchSize)
+
+    # get model
+    model = VIB( z_dim=z_dim)
+
+    # get optimizer
+    optimizer = Adam( model.parameters, lr=lr, betas=(.5,.999))
+
+    for epi in range(MaxEpoch):
+
+        epoch_loss = 0
+        for i, batch in enumerate(train_data):
+            x_batch, y_batch = batch
+            x_batch, y_batch = x_batch.to(model.device), y_batch.to(model.device)
+            # forward 
+            p_Y1Z, p_Y1X, mu, logsig = model.forward( x_batch)
+            loss = model.get_loss( p_Y1Z, p_Y1X, mu, logsig, beta=beta)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+
+        if (i+1) % logfreq ==0:
+            print( f'Epoch: {epi}, Loss: {epoch_loss:.3f}')
+
+
 
 if __name__ == '__main__':
 
